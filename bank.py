358fa856f229be5fb84ea18e0db4261e4073d939
rahul-72 <charan7rahul@gmail.com>
2019-04-10 20:39:56 +0530

    initial tkinter framework

--- bank.py
+++ bank.py
@@ -1,20 +1,11 @@
 import tkinter as tk
 
 root=tk.Tk()
+root.geometry("400x400")
+root.minsize(300,300)
 
-#we can create only one root object.
-"""we have to create a Tk root widget, which is a window with a title bar and 
-other decoration provided by the window manager. 
-The root widget has to be created before any other widgets and 
-there can only be one root widget."""
 
-w=tk.Label(root, text="Hello tkinter")
 
-"""The next line of code contains the Label widget. 
-The first parameter of the Label call is the name of the parent window, 
-in our case "root". So our Label widget is a child of the root widget. 
-The keyword parameter "text" specifies the text to be shown:"""
 
-w.pack()
 
 root.mainloop()
